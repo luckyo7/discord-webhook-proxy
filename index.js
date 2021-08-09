bb3e8041dfa31f0b34e32d2db3f9856ac0542bf4
GardenGnome164 <evankiess@gmail.com>
2021-08-09 10:37:48 -0400

    Changed from URL params to JSON body params

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var XMLHttpRequest = _XMLHttpRequest.XMLHttpRequest;
 
 var router = express.Router();
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.post("/post", (req, res, next) => {
+    var message = req.body.message || req.query.message
+    var webhook = req.body.webhook || req.query.webhook
+    var username = req.body.username || req.query.username || "Discord Integration"
+    var avatar = req.body.avatar || req.query.avatar || "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Garry%27s_Mod_logo.svg/768px-Garry%27s_Mod_logo.svg.png"
     // make sure they supplied everything needed
-    if (!(req.query.message && req.query.webhook)) {
+    if (!(message && webhook)) {
         res.status(400).send("Invalid Data. Missing message or webhook."); // return error status code
         return;
     } 
-    var message = req.query.message;
-    var webhook = "https://discord.com/api/webhooks/" + req.query.webhook; // webhook part should look like 863462515631849532/su8Udm1C-zorau4d3wV3Rq303TQ95uAWS7vHdVwy1r7DXFeMPqTKK9x9BIrAtxWMSCi7
-    var username = (req.query.username && req.query.username != "") ? req.query.username : "Discord Integration"
-    var avatar = (req.query.avatar && req.query.avatar != "") ? req.query.avatar : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Garry%27s_Mod_logo.svg/768px-Garry%27s_Mod_logo.svg.png"
+    webhook = "https://discord.com/api/webhooks/" + webhook; // webhook part should look like 863462515631849532/su8Udm1C-zorau4d3wV3Rq303TQ95uAWS7vHdVwy1r7DXFeMPqTKK9x9BIrAtxWMSCi7
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", webhook, true);
